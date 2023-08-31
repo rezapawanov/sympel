fa690f12372e2ebbdce5d276ccddfa1fe97bb6e7
rezapawanov <rezapawanov@gmail.com>
2023-09-01 00:41:37 +0700

    synx

--- keuangan/application/views/top.php
+++ keuangan/application/views/top.php
@@ -32,7 +32,7 @@ $sekolah = $this->db->query("SELECT * FROM mst_sekolah WHERE id = 1")->row();
   <!-- Font Awesome Pro -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"> -->
   <!-- Select2 -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/select2/css/select2.min.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
@@ -50,15 +50,15 @@ $sekolah = $this->db->query("SELECT * FROM mst_sekolah WHERE id = 1")->row();
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/adminlte.min.css">
 
 
-  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.css">
-  <link href="<?php echo base_url(); ?>asset/fullcalendar.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/bootstrap-datepicker.css">
+  <link href="<?php echo base_url(); ?>assets/dist/css/fullcalendar.css" rel="stylesheet">
 
   <!-- jQuery -->
   <script src="<?php echo base_url(); ?>assets/plugins/jquery/jquery.min.js"></script>
-  <script type="text/javascript" src="<?php echo base_url(); ?>asset/typeahead.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>assets/dist/js/typeahead.js"></script>
   <script src="https://code.highcharts.com/highcharts.js"></script>
-  <script src="<?php echo base_url(); ?>asset/fullcalendar.js"></script>
-  <script src="<?php echo base_url(); ?>asset/dist/js/jquery.tabledit.js"></script>
+  <!-- script src="<?php echo base_url(); ?>assets/dist/js/fullcalendar.js"></script> -->
+  <script src="<?php echo base_url(); ?>assets/dist/js/jquery.tabledit.js"></script>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed ">
