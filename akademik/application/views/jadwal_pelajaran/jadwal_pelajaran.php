13ad6feb33fd7fff310ef37d74ceb8eaa4a72ac7
fauziahmad37 <fauzi.enginer@gmail.com>
2023-10-09 22:24:24 +0700

    hide tablist hari jadwal pelajaran

--- akademik/application/views/jadwal_pelajaran/jadwal_pelajaran.php
+++ akademik/application/views/jadwal_pelajaran/jadwal_pelajaran.php
@@ -101,7 +101,7 @@
 
 
 
-              <ul class="nav nav-tabs" id="myTab" role="tablist">
+              <ul class="nav nav-tabs d-none" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
                   <button class="nav-link active" id="senin-tab" data-toggle="tab" data-target="#senin" type="button" role="tab" aria-controls="senin" aria-selected="true">senin</button>
                 </li>
@@ -118,7 +118,7 @@
                   <button class="nav-link" id="jumat-tab" data-toggle="tab" data-target="#jumat" type="button" role="tab" aria-controls="jumat" aria-selected="false">jumat</button>
                 </li>
               </ul>
-              <div class="tab-content" id="myTabContent">
+              <div class="tab-content d-none" id="myTabContent">
                 <div class="tab-pane fade show active" id="senin" role="tabpanel" aria-labelledby="senin-tab">
                   <table id="seninTable" class="table table-hover table-striped mt-3">
                     <thead>
