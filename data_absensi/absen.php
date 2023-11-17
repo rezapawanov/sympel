c739c42f8a458a7627693c591b5c2b91322f480c
rezapawanov <rezapawanov@gmail.com>
2023-11-18 00:10:53 +0700

    tambah absen

--- data_absensi/absen.php
+++ data_absensi/absen.php
@@ -8,6 +8,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $query = "INSERT INTO rfid (tag, ket) VALUES ('$tag', '$ket')";
     mysqli_query($koneksi, $query);
+    
+    $query = "SELECT id_siswa, id_kelas FROM mst_siswa WHERE rfid = '$tag'";
+    $result = mysqli_query($koneksi, $query);
+    
+    if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    $id_siswa = $row['id_siswa'];
+    $id_kelas = $row['id_kelas'];
+    $currentDate = date("Y-m-d");
+
+    // Now, you can use the retrieved "tag" value in your INSERT query
+    $query = "insert into absen(id_siswa, id_kelas, tahun_ajaran, keterangan, tanggal_absen) values ('$id_siswa', '$id_kelas', '2023/2024','$ket','$currentDate') ";
+    mysqli_query($koneksi, $query);
+    }
+
+    
+   
 } else {
     // Handle cases where the request method is not POST
     echo "Invalid request method.";
