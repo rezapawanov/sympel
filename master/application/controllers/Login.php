cb817c924b3738276780506edb555333bf7c83d4
rezapawanov <rezapawanov@gmail.com>
2024-01-12 02:54:24 +0700

    sync

--- master/application/controllers/Login.php
+++ master/application/controllers/Login.php
@@ -12,6 +12,7 @@ class Login extends CI_Controller {
 		if($this->input->method(TRUE) == 'POST' && !empty($_POST)) {
 			$in['username'] = $this->input->post('username');
 			$in['password'] = md5($this->input->post('password'));
+			echo var_dump($in['password']);
 			$in['jenis'] = $this->input->post('jenis');
 			$this->Login_model->cek($in);
 			
