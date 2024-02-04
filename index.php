e8b5761911474dcd295f95ef313400b6a9562a88
rezapawanov <rezapawanov@gmail.com>
2024-02-04 13:09:01 +0700

    sync

--- index.php
+++ index.php
@@ -14,7 +14,9 @@
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
@@ -67,7 +69,8 @@
           <h2 data-aos="fade-up" data-aos-delay="400">Satu Aplikasi Satu Untuk Semua</h2>
           <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
-              <a href="master" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+              <a href="master"
+                class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                 <span>Login</span>
                 <i class="bi bi-arrow-right"></i>
               </a>
@@ -83,63 +86,66 @@
   </section><!-- End Hero -->
   <section id="services" class="services">
 
-<div class="container aos-init aos-animate" data-aos="fade-up">
+    <div class="container aos-init aos-animate" data-aos="fade-up">
 
-  <header class="section-header">
-	<h2>PORTAL</h2>
-	<p>DAFTAR LAYANAN PORTAL SYMPEL</p>
-  </header>
+      <header class="section-header">
+        <h2>PORTAL</h2>
+        <p>DAFTAR LAYANAN PORTAL SYMPEL</p>
+      </header>
 
-  <div class="row gy-4">
+      <div class="row gy-4">
 
-	<div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-	  <div class="service-box blue">
-		<i class="ri-user-line icon"></i>
-		<h3>PPDB</h3>
-		<p>Penerimaan Siswa Didik Baru</p>
-		<a href="ppdb/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
-	  </div>
-	</div>
-	<div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-	  <div class="service-box red">
-		<i class="ri-git-repository-fill icon"></i>
-		<h3>PERPUSTAKAAN</h3>
-		<p>Daftar Katalog Buku</p>
-		<a href="ppdb/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
-	  </div>
-	</div>
-	<div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-	  <div class="service-box green">
-		<i class="ri-graduation-cap-line icon"></i>
-		<h3>KELULUSAN</h3>
-		<p>Daftar Pengumuman Kelulusan</p>
-		<a href="kelulusan/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
-	  </div>
-	</div>
-	<div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-	  <div class="service-box orange">
-		<i class="ri-passport-line icon"></i>
-		<h3>ALUMNI</h3>
-		<p>Daftar Informasi Alumni</p>
-		<a href="alumni/app" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
-	  </div>
-	</div>
-	<div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-	  <div class="service-box purple">
-		<i class="ri-user-star-line icon"></i>
-		<h3>BUKU TAMU</h3>
-		<p>Daftar Kunjungan Buku Tamu</p>
-		<a href="bukutamu/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
-	  </div>
-	</div>
-	<div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
-	  <div class="service-box pink">
-		<i class="ri-mac-line icon"></i>
-		<h3>CBT</h3>
-		<p>Computer Based Test</p>
-		<a href="cbt/manager" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
-	  </div>
-	</div>
+        <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+          <div class="service-box blue">
+            <i class="ri-user-line icon"></i>
+            <h3>PPDB</h3>
+            <p>Penerimaan Siswa Didik Baru</p>
+            <a href="ppdb/portal" target="_blank" class="read-more"><span>Read More</span> <i
+                class="bi bi-arrow-right"></i></a>
+          </div>
+        </div>
+        <!-- 
+  <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+    <div class="service-box red">
+    <i class="ri-git-repository-fill icon"></i>
+    <h3>PERPUSTAKAAN</h3>
+    <p>Daftar Katalog Buku</p>
+    <a href="ppdb/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+    </div>
+  </div>
+  -->
+  <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+    <div class="service-box green">
+    <i class="ri-graduation-cap-line icon"></i>
+    <h3>KELULUSAN</h3>
+    <p>Daftar Pengumuman Kelulusan</p>
+    <a href="kelulusan/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+    <div class="service-box orange">
+    <i class="ri-passport-line icon"></i>
+    <h3>ALUMNI</h3>
+    <p>Daftar Informasi Alumni</p>
+    <a href="alumni/app" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+    <div class="service-box purple">
+    <i class="ri-user-star-line icon"></i>
+    <h3>BUKU TAMU</h3>
+    <p>Daftar Kunjungan Buku Tamu</p>
+    <a href="bukutamu/portal" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+    <div class="service-box pink">
+    <i class="ri-mac-line icon"></i>
+    <h3>CBT</h3>
+    <p>Computer Based Test</p>
+    <a href="cbt/manager" target="_blank" class="read-more"><span>Read More</span> <i class="bi bi-arrow-right"></i></a>
+    </div>
+  </div>
 
 
   </div>
@@ -148,29 +154,30 @@
 
 </section>
   <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+        <footer id="footer" class="footer">
 
 
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>SYMPEL</span></strong>. All Rights Reserved
-      </div>
-    </div>
-  </footer><!-- End Footer -->
+          <div class="container">
+            <div class="copyright">
+              &copy; Copyright <strong><span>SYMPEL</span></strong>. All Rights Reserved
+            </div>
+          </div>
+        </footer><!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+        <!-- Vendor JS Files -->
+        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="assets/vendor/aos/aos.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
+        <!-- Template Main JS File -->
+        <script src="assets/js/main.js"></script>
 
 </body>
 
