a657119ddc7a9b8ffc5bacfaac1ef631ed944817
rezapawanov <rezapawanov@gmail.com>
2024-02-04 12:35:31 +0700

    sync

--- data_absensi/current_url.php
+++ data_absensi/current_url.php
@@ -1,4 +1,5 @@
 <?php
-    // Handle cases where the request method is not POST
-    echo $_SERVER['SERVER_NAME'];
+// Handle cases where the request method is not POST
+echo $_SERVER['SERVER_NAME'];
+echo $_SERVER['REQUEST_URI'];
 ?>
